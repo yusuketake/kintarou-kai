c47cad2a969fab1fa4d3da1c8118f119eefd2c2c
yusuketake <yusuketake62@gmail.com>
2023-12-31 21:17:54 +0900

    divタグ削除

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -2,12 +2,12 @@ import "./style.css";
 
 export default function Header() {
   return (
-    <div>
+    <>
       <div className="flex justify-between bg-gray-200 p-5">
         <div>名前</div>
         <div>部署</div>
         <div>ログアウト</div>
       </div>
-    </div>
+    </>
   );
 }
