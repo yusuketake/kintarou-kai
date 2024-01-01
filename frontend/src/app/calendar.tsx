9508f6d147039e54d70ed0a37d81b0bbc5e5c1fd
yusuketake <yusuketake62@gmail.com>
2024-01-01 22:42:42 +0900

    fullcalendarをinteractiveに変更

--- frontend/src/app/calendar.tsx
+++ frontend/src/app/calendar.tsx
@@ -1,15 +1,34 @@
 "use client";
 
+import React from "react";
 import "./style.css";
+import { Calendar } from "@fullcalendar/core";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-function Calendar() {
-  return (
-    <div>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
-    </div>
-  );
+class DisplayCalendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          dateClick={this.handleDateClick}
+        />
+      </div>
+    );
+  }
+
+  handleDateClick = (info: { dateStr: string }) => {
+    alert("Clicked on: " + info.dateStr);
+
+    let date = info.dateStr;
+
+    // APIにpostしてその日のAttendanceを取得して表示する
+  };
 }
 
-export default Calendar;
+export default DisplayCalendar;
+
+// https://fullcalendar.io/docs/react
