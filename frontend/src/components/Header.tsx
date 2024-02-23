c0c388f2fc25068752ac7f32a8e44c240c82b7e5
yusuketake <yusuketake62@gmail.com>
2024-02-23 18:41:13 +0900

    formatter適用

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
-'use client';
-import Image from 'next/image';
-import kintarou from './kintarou.png';
-import { useRouter } from 'next/navigation';
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import kintarou from "./kintarou.png";
 
 export type User = {
   id: number;
@@ -21,17 +21,17 @@ export const Header = (props: Props) => {
   return (
     <div className="bg-blue-500 text-white pt-3">
       <div className="flex justify-center items-center">
-        <Image src={kintarou} alt={''} width={60}></Image>
+        <Image src={kintarou} alt={""} width={60}></Image>
         <div className="ml-5 text-lg font-bold">勤太郎改</div>
       </div>
       <div className="flex justify-between p-3 font-semibold items-center">
-        <span>{user ? user.name : ''}</span>
-        <span>{user ? user.department : ''}</span>
+        <span>{user ? user.name : ""}</span>
+        <span>{user ? user.department : ""}</span>
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           onClick={() => {
-            localStorage.removeItem('token');
-            router.push('/login');
+            localStorage.removeItem("token");
+            router.push("/Login");
           }}
         >
           ログアウト
